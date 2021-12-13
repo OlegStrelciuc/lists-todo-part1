fdca4ffd6e68aa0ba68d657ecef1d3b12c22a3ab
OlegStrelciuc <93653731+OlegStrelciuc@users.noreply.github.com>
2021-12-13 16:28:55 +0200

    screens only with results

---
+++ todo.part1.py
@@ -0,0 +1,72 @@
+from os import system
+tasks = []
+
+
+# MAIN LOOP #################################
+while True:
+    system("cls")
+    print("MENU")
+    print("1. add task")
+    print("2. show tasks")
+    print("3. remove task")
+    print("4. change task")
+    print("5. swap tasks")
+    print("0.Exit")
+    option = int(input(">> "))
+    if option == 1:
+        system("cls")
+        # ADD TASKS #################################
+        while True:
+            new_task = input("Name your next task: ")
+            if new_task == "":
+                break
+
+            if new_task not in tasks:
+                tasks.append(new_task)
+
+
+    if option == 2:
+        system("cls")
+        # PRINT TASKS ###############################
+        print("\nYour tasks: ")
+
+        for i in range(len(tasks)):
+            print(i+1, " > ", tasks[i])
+
+        input("\nHit ENTER to continue")
+
+
+    if option == 3:
+        system("cls")
+        # EDIT A TASK ###############################
+        index = int(input("Enter task position: ")) - 1
+        print("Are you sure you want to remove this task? : <<", tasks[index], ">>")
+        rem_task = input("\nyes/no: ")
+        if rem_task == "yes":
+            tasks.pop(index)
+        else:
+            break
+
+
+    if option == 4:
+        system("cls")
+        # EDIT A TASK ###############################
+        index = int(input("Enter task position: ")) - 1
+        new_task = input("Enter new task title: ")
+        tasks[index] = new_task
+
+
+    if option == 5:
+        system("cls")
+        index_A = int(input("Enter first task position: ")) - 1
+        index_B = int(input("Enter second task position: ")) - 1
+        st = tasks.pop(index_A)                                    # st - swap tasks
+        st_1 = tasks.pop(index_B-1)
+        tasks.insert(index_B, st)
+        tasks.insert(index_A, st_1)
+
+
+
+
+    if option == 0:
+        break
